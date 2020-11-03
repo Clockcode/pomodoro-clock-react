1b1a06f29d594e54cd03f717d782ad7a01f9e0de
Clockcode <cagdasmuldur@gmail.com>
2020-11-02 22:57:26 -0500

    created build folder

---
+++ build/precache-manifest.36c38adc39f7af066d61a5b1872563c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed1835339794f4e9bd27ad082f4458b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed53ef3af6264aef6ef",
+    "url": "/static/css/main.3e669964.chunk.css"
+  },
+  {
+    "revision": "3db86eaec2fa9ff1f73e",
+    "url": "/static/js/2.0ded1434.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.0ded1434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed53ef3af6264aef6ef",
+    "url": "/static/js/main.979332c3.chunk.js"
+  },
+  {
+    "revision": "f80a0df1464b0afe19e4",
+    "url": "/static/js/runtime-main.c3654c26.js"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/static/media/BeepSound.988833bf.wav"
+  },
+  {
+    "revision": "87af1751caf7945ccca5ce2909af9309",
+    "url": "/static/media/Commissioner-VariableFont_wght.87af1751.ttf"
+  },
+  {
+    "revision": "490cb0e45f39580c7ba1b856f2f1f4a7",
+    "url": "/static/media/Righteous-Regular.490cb0e4.ttf"
+  }
+]);
